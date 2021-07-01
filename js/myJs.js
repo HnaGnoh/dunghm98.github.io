b5db86d1066534ac0d37a54c846a0b8b04126c0c
HnaGnoh <86766676+HnaGnoh@users.noreply.github.com>
2021-07-01 14:51:53 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -14,9 +14,9 @@ function firstQuestion(){
     
     $('.content').hide();
     Swal.fire({
-        title: 'He luu cậu!',
-        text: 'Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.',
-        imageUrl: '../img/cuteCat.jpg',
+        title: 'Anoo Ẻm Kutee ❤️!',
+        text: 'T có điều này muốn hỏi mài nhớ phải trả lời thật lòng nhaaa.',
+        imageUrl: '../img/got_1909846425848029.jpeg',
         imageWidth: 300,
         imageHeight: 300,
         background: '#fff url("../img/iput-bg.jpg")',
@@ -73,7 +73,7 @@ $('#no').click(() => {
 // generate text in input
 function textGenerate() {
     var n = "";
-    var text = " Tại vì cậu đẹp trai vl :<<<<<<< ";
+    var text = " T cũng thích mài vl á :<<<<<<< ";
     var a = Array.from(text);
     var textVal = $('#txtReason').val() ? $('#txtReason').val() : "";
     var count = textVal.length;
@@ -96,7 +96,7 @@ $('#yes').click(function() {
     var audio = new Audio('../sound/tick.mp3');
     audio.play();
     Swal.fire({
-        title: 'Nói cho tớ lí do cậu thích tớ đi :vvvv',
+        title: 'Nói cho t lí do mài thích t đi :vvvv',
         html: true,
         width: 900,
         padding: '3em',
@@ -120,12 +120,12 @@ $('#yes').click(function() {
             Swal.fire({
                 width: 900,
                 confirmButtonText: 'Okiiiii lun <3',
-                background: '#fff url("../img/iput-bg.jpg")',
-                title: 'Tớ biết mà ^^ Yêu cậu 300.000',
-                text: "Tối nay tớ qua đón cậu đi chơi nhaaaaaaaaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàoooooo",
+                background: '#fff url("../img/IMG_20210417_201606.jpg")',
+                title: 'Anh biết mà ^^ Yêu Em 300.000',
+                text: "Tối nay t qua đón mài đi chơi nhaaaaaaaaa :v Còn giờ thì chờ gì nữa mà ko inbox cho t đi nàoooooo",
                 confirmButtonColor: '#83d0c9',
                 onClose: () => {
-                    window.location = 'http://fb.com';
+                    window.location = 'http://facebook.com/LT.HongAnh.269';
                   }
             })
         }
